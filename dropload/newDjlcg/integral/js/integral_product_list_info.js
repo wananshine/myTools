d9d693c737a10fcb276b0df0fcaf137e53d74314
zengts <wananshine@Hotmail.com>
2017-11-25 12:05:50 +0800

    update

--- dropload/newDjlcg/integral/js/integral_product_list_info.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017/4/11 0011.
- */
-$(function(){
-    //分别加载页眉，头部，页脚，侧边栏
-    $("#titleTemp_main").load("../lib/temp/page_title.html");
-    $("#bottomTemp_main").load("../lib/temp/page_bottom.html");
-    MoveImg();
-});
